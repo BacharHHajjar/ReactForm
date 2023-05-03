f2212aca20f2ae4aaec36583baea86335bff6b85
Bachar HAJJAR <bachar.hajjar@amadeus.com>
2023-05-03 09:58:28 +0200

    Code refactored, easier to read

--- src/App.js
+++ src/App.js
@@ -28,48 +28,6 @@ function App() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showFailedMessage, setShowFailedMessage] = useState(false);
 
-  const inputs = [
-    {
-      id: 1,
-      name: "ccnumber",
-      type: "text",
-      placeholder: "1234 5678 9101 1213",
-      errormessage: "Should be a valid credit card number (16 numbers)",
-      label: "Card Number",
-      pattern: "^[0-9]{16}$",
-      required: false,
-    },
-    {
-      id: 2,
-      name: "nameOnCard",
-      type: "text",
-      placeholder: "Ex. John Smith",
-      errormessage: "Name should be only letters between 2 and 20",
-      pattern: "^[A-Za-z ]{2,20}$",
-      label: "Name on card",
-      required: false,
-    },
-    {
-      id: 3,
-      name: "expDate",
-      type: "text",
-      placeholder: "01/25",
-      pattern: "^(0[1-9]|1[0-2])/?([0-9]{2})$",
-      errormessage: "Invalid date or date older than today's",
-      label: "Expiry date",
-      required: false,
-    },
-    {
-      id: 4,
-      name: "secCode",
-      type: "text",
-      placeholder: "CVV",
-      errormessage: "Security code should be three numbers",
-      label: "Security Code",
-      pattern: "^[0-9]{3}$",
-      required: false,
-    },
-  ];
   const handleSubmit = (e) => {
     e.preventDefault();
     setShowLoader(true);
@@ -109,14 +67,51 @@ function App() {
       <div className="app">
         <form onSubmit={handleSubmit}>
           <h1>Checkout</h1>
-          {inputs.map((input) => (
-            <Input
-              key={input.id}
-              {...input}
-              value={values[input.name]}
-              onChange={onChange}
-            />
-          ))}
+
+          <Input
+            name="ccnumber"
+            key="1"
+            onChange={onChange}
+            placeholder="1234 5678 9101 1213"
+            errormessage="Should be a valid credit card number (16 numbers)"
+            label="Card Number"
+            pattern="^[0-9]{16}$"
+            required={false}
+          />
+
+          <Input
+            name="nameOnCard"
+            key="2"
+            onChange={onChange}
+            placeholder="Ex. John Smith"
+            errormessage="Name should be only letters between 2 and 20"
+            label="Name on card"
+            pattern="^[A-Za-z ]{2,20}$"
+            required={false}
+          />
+
+          <Input
+            name="expDate"
+            key="3"
+            onChange={onChange}
+            placeholder="01/25"
+            errormessage="Invalid date or date older than today's"
+            label="Expiry date"
+            pattern="^(0[1-9]|1[0-2])/?([0-9]{2})$"
+            required={false}
+          />
+
+          <Input
+            name="secCode"
+            key="4"
+            onChange={onChange}
+            placeholder="CVV"
+            errormessage="Security code should be three numbers"
+            label="Security Code"
+            pattern="^[0-9]{3}$"
+            required={false}
+          />
+
           <Button
             loading={showLoader}
             text="Pay"
